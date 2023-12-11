f37b3ca70056472cebd5db378ad59414bd10f932
bjosefine <josefinebrolin95@gmail.com>
2023-12-11 18:50:50 +0100

    route and tailwind fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,29 @@
-import "./App.css";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
-export const App = () => {
+import { Home } from "./pages/Home";
+import { Products } from "./pages/Products";
+import { Navbar } from "./components/Navbar";
+
+function Root() {
   return (
     <>
-      <p>Hola frontend!</p>
+      <Navbar />
+      <main className="bg-orange-500">
+        <Outlet />
+      </main>
     </>
   );
+}
+
+export const App = () => {
+  const router = createBrowserRouter([
+    {
+      children: [
+        { element: <Home />, path: "/" },
+        { element: <Products />, path: "/products" },
+      ],
+      element: <Root />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 };
